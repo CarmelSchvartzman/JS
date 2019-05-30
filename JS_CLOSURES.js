59cd9b7224a469a8899c032efe465291d9b618dd
Carmel Shvartzman <carmel.aspnet@gmail.com>
2019-05-30 14:03:03 +0300

    Update JS_CLOSURES.js

--- JS_CLOSURES.js
+++ JS_CLOSURES.js
@@ -1,8 +1,9 @@
 var add = (function () {
-  var counter = 0;
+  var counter = 0;  // PRIVATE VARIABLE
   return function () {counter += 1; return counter}
 })();
 
+// AFTER RUNNING ONLY ONCE, self-invoking function "ADD" BECOMES A FUNCTION to be called:
 add();
 add();
 add();
