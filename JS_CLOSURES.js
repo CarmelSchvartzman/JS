ff185d0fab03813f73996e103c7ae8fc3e2b0c65
Carmel Shvartzman <carmel.aspnet@gmail.com>
2019-05-30 14:01:30 +0300

    Create JS_CLOSURES.js

---
+++ JS_CLOSURES.js
@@ -0,0 +1,10 @@
+var add = (function () {
+  var counter = 0;
+  return function () {counter += 1; return counter}
+})();
+
+add();
+add();
+add();
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
