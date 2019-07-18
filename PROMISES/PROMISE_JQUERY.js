3bf70b2c0e16896a1ba3c11ead28d1b06ab91c0d
Carmel Shvartzman <carmel.aspnet@gmail.com>
2019-07-18 11:08:43 +0300

    Create PROMISE_JQUERY.js

---
+++ PROMISES/PROMISE_JQUERY.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $("button").click(function(){
+    
+    fnPromise('Hello').then((url) => { alert('DATA = ' + url); },(e) => { alert('ERROR = ' + e); });
+    
+  });
+});
+
+const fnPromise = (url) => { 
+
+	return new Promise((resolve,reject) => {    
+    	resolve(url);
+    }
+    );
+}
+
+</script>
+</head>
+<body>
+
+<button>Call a Promise and get the result back</button>
+
+</body>
+</html>
