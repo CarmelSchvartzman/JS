7513fc9df75723b1cd86d6c692f5278a2763f238
Carmel Shvartzman <carmel.aspnet@gmail.com>
2019-08-11 08:39:01 +0300

    Update USEFUL_CODE.js

--- USEFUL_CODE.js
+++ USEFUL_CODE.js
@@ -8,4 +8,14 @@ const cloned = JSON.parse(JSON.stringify( OldObj ));
 let cloned = {};
 $.extend(cloned, OldObj);
 
+/* example :
+    let orig = {id:3,name:'dan'};
+    let clone = {};
+    $.extend(clone,orig);      <<<<<<<<<<<<<<<<<<<<<<<<<
+    orig.name = 'CHANGED';
+    $('p').html(JSON.stringify(clone));
+*/
+/////////////////////////////////////////////////////////////////////////////////
+
+
 
