692d0b71d3852a8c9622140e24497f65429204b4
Carmel Shvartzman <carmel.aspnet@gmail.com>
2019-08-11 09:52:05 +0300

    Update USEFUL_CODE.js

--- USEFUL_CODE.js
+++ USEFUL_CODE.js
@@ -1,37 +1,3 @@
-////  DEEP COPY :
+///////  CODE UTILE :
 
 
-const cloned = JSON.parse(JSON.stringify( OldObj ));
-
-/* EXAMPLE :
-let a = [{ x:{z:1} , y: 2}];
-let b = JSON.parse(JSON.stringify(a));
-b[0].x.z=0
-console.log(JSON.stringify(a)); //[{"x":{"z":1},"y":2}]
-console.log(JSON.stringify(b)); // [{"x":{"z":0},"y":2}]
-*/
-
-///  OR :
-let cloned = {};
-$.extend(cloned, OldObj);
-
-/* example :
-    let orig = {id:3,name:'dan'};
-    let clone = {};
-    $.extend(true, clone,orig);      <<<<<<<<<<<<<<<<<<<<<<<<<
-    orig.name = 'CHANGED';
-    $('p').html(JSON.stringify(clone));
-*/
-
-let a = [{ x:{z:1} , y: 2}];
-let b = $.extend(true, [], a);
-b[0].x.z=0
-console.log(JSON.stringify(a)); //[{"x":{"z":1},"y":2}]
-console.log(JSON.stringify(b)); // [{"x":{"z":0},"y":2}]
-/////////////////////////////////////////////////////////////////////////////////
-
-////  SHALLOW COPY :
-
-const cloned = Object.assign({}, OldObj);
-
-/////////////////////////////////////////////////////////////////////////////////
